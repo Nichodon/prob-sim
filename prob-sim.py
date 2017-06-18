f0c0b7164a1ceae635e514764e0a55fa7a39470e
Nichodon <brandonsyc.2222@yahoo.com>
2017-06-18 15:42:50 -0700

    tweak aesthetics

--- prob-sim.py
+++ prob-sim.py
@@ -42,7 +42,7 @@ def range_(array, limit):
 
 colors = ['#09c', '#90c']
 # p1hp, p2dm p1dd
-data = [curve(300.0, 30.0, 0.1), curve(300.0, 100.0, 0.5)]
+data = [curve(300.0, 30.0, 0.65), curve(300.0, 100.0, 0.9)]
 minimum = min(min(data[0][1]), min(data[1][1]))
 maximum = max(max(data[0][1]), max(data[1][1]))
 
@@ -52,10 +52,10 @@ tk.wm_title('probsim')
 width = (maximum - minimum) * 10
 height = max(max(data[0][0]), max(data[1][0])) * 1000
 
-lf1 = LabelFrame(master=tk, text="Graph")
+lf1 = LabelFrame(master=tk, text='Graph')
 lf1.grid(row=0, column=1, padx=20, pady=10, sticky=NS)
 canvas = Canvas(master=lf1, width=width + 30, height=height + 30)
-canvas.grid(row=0, column=0, padx=10, pady=10)
+canvas.pack(padx=20, pady=10, side=BOTTOM)
 
 # Horizontal
 for i in range(0, int(height / 10), 5):
@@ -65,12 +65,18 @@ for i in range(int(minimum), int(maximum), 5):
     canvas.create_text(i * 10 - minimum * 10 + 10, height + 20, text=i)
     canvas.create_line(i * 10 - minimum * 10 + 10, 10, i * 10 - minimum * 10 + 10, height + 10, fill="#ccc")
 for i in range(len(data)):
+    x1 = data[i][1][0] * 10 - minimum * 10 + 10
+    y1 = height - data[i][0][0] * 1000 + 10
+    canvas.create_oval(x1 - 1, y1 - 1, x1 + 1, y1 + 1, fill=colors[i], outline='')
     for j in range(len(data[i][0]) - 1):
         x1 = data[i][1][j] * 10 - minimum * 10 + 10
         y1 = height - data[i][0][j] * 1000 + 10
         x2 = data[i][1][j + 1] * 10 - minimum * 10 + 10
         y2 = height - data[i][0][j + 1] * 1000 + 10
         canvas.create_line(x1, y1, x2, y2, width=3, fill=colors[i])
+    x1 = data[i][1][-1] * 10 - minimum * 10 + 10
+    y1 = height - data[i][0][-1] * 1000 + 10
+    canvas.create_oval(x1 - 1, y1 - 1, x1 + 1, y1 + 1, fill=colors[i], outline='')
 
 final = []
 
@@ -102,22 +108,21 @@ for i in range(int(min(data[0][1][0], data[1][1][0])), int(max(data[0][1][-1], d
 
 final.append(r)
 
-print final
 final = [x * 1 / sum(final) for x in final]
 tk.update_idletasks()
 
-stretch = canvas.winfo_height()
+stretch = max(canvas.winfo_height(), 200)
 lf3 = LabelFrame(master=tk, text="Approximate Results")
 lf3.grid(row=0, column=3, padx=20, pady=10, sticky=NS)
 graph = Canvas(master=lf3, width=190, height=stretch)
-graph.grid(row=0, column=0, padx=20, pady=10)
+graph.pack(padx=20, pady=10, side=BOTTOM)
 for i in range(11):
     graph.create_line(0, i * (stretch - 40) / 10 + 40, 180, i * (stretch - 40) / 10 + 40, fill="#ccc")
 graph.create_rectangle(10, stretch - (stretch - 40) * final[2], 60, stretch, fill=colors[0], outline='')
 graph.create_text(35, 20, text="Blue\n" + str(round(final[2] * 100, 2)) + '%')
-graph.create_rectangle(70, stretch - (stretch - 40) * final[1], 120, canvas.winfo_height(), fill=colors[1], outline='')
+graph.create_rectangle(70, stretch - (stretch - 40) * final[1], 120, stretch, fill=colors[1], outline='')
 graph.create_text(95, 20, text="Purple\n" + str(round(final[1] * 100, 2)) + '%')
-graph.create_rectangle(130, stretch - (stretch - 40) * final[0], 180, canvas.winfo_height(), fill='#999', outline='')
+graph.create_rectangle(130, stretch - (stretch - 40) * final[0], 180, stretch, fill='#999', outline='')
 graph.create_text(155, 20, text="Tie\n" + str(round(final[0] * 100, 2)) + '%')
 
 
