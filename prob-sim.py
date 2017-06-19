3386b5bed5ffb4f165c4906c854ee9eaa8374694
Nichodon <brandonsyc.2222@yahoo.com>
2017-06-18 20:15:08 -0700

    add armor

--- prob-sim.py
+++ prob-sim.py
@@ -8,7 +8,7 @@ import ttk
 tk = Tk()
 tk.wm_title('probsim')
 
-inputs = [500, 100, 0.5, 300, 200, 0.7]
+inputs = [500, 100, 0.5, 0.3, 0.9, 300, 200, 0.7, 0.7, 0.1]
 
 
 def curve(hp, dmg, ddg):
@@ -53,8 +53,8 @@ def update():
     global inputs, canvas, graph, lf1, lf3
 
     try:
-        inputs = [float(x.get()) for x in [e1, e2, e3, e4, e5, e6]]
-        if inputs[2] > 1 or inputs[5] > 1:
+        inputs = [float(x.get()) for x in [e1, e2, e3, e4, e5, e6, e7, e8, e9, e10]]
+        if inputs[2] > 1 or inputs[7] > 1:
             return
     except ValueError:
         pass
@@ -80,7 +80,9 @@ def update():
                 # Throws error
     '''
     try:
-        data = [curve(inputs[0], inputs[4], inputs[2]), curve(inputs[3], inputs[1], inputs[5])]
+        d1 = inputs[6] * (1 - max(inputs[3] - inputs[9], 0))
+        d2 = inputs[1] * (1 - max(inputs[8] - inputs[4], 0))
+        data = [curve(inputs[0], d1, inputs[2]), curve(inputs[5], d2, inputs[7])]
     except ValueError:
         return
 
@@ -171,11 +173,11 @@ lf2.grid(row=0, column=0, padx=10, pady=10, sticky=NS)
 f1 = Frame(master=lf2)
 f1.pack(expand=True)
 s1 = ttk.Separator(master=f1)
-s1.grid(row=4, column=0, columnspan=2, sticky=EW, padx=10)
+s1.grid(row=6, column=0, columnspan=2, sticky=EW, padx=10)
 s2 = ttk.Separator(master=f1)
-s2.grid(row=9, column=0, columnspan=2, sticky=EW, padx=10)
+s2.grid(row=12, column=0, columnspan=2, sticky=EW, padx=10)
 b1 = Button(master=f1, text="Update", command=update, padx=10, pady=5, relief=GROOVE)
-b1.grid(row=10, column=0, sticky=W, padx=10, pady=10)
+b1.grid(row=13, column=0, sticky=W, padx=10, pady=10)
 
 e1 = Entry(master=f1, vcmd=command, highlightbackground='#ccc', highlightthickness=1, bd=0)
 e1.grid(row=1, column=1, padx=10, pady=10)
@@ -183,28 +185,44 @@ e2 = Entry(master=f1, vcmd=command, highlightbackground='#ccc', highlightthickne
 e2.grid(row=2, column=1, padx=10, pady=10)
 e3 = Entry(master=f1, vcmd=command, highlightbackground='#ccc', highlightthickness=1, bd=0)
 e3.grid(row=3, column=1, padx=10, pady=10)
+e4 = Entry(master=f1, vcmd=command, highlightbackground='#ccc', highlightthickness=1, bd=0)
+e4.grid(row=4, column=1, padx=10, pady=10)
+e5 = Entry(master=f1, vcmd=command, highlightbackground='#ccc', highlightthickness=1, bd=0)
+e5.grid(row=5, column=1, padx=10, pady=10)
 l1 = Label(master=f1, text="Health Points", foreground=colors[0])
 l1.grid(row=1, column=0, padx=10, pady=10, sticky=W)
 l2 = Label(master=f1, text="Attack Damage", foreground=colors[0])
 l2.grid(row=2, column=0, padx=10, pady=10, sticky=W)
 l3 = Label(master=f1, text="Dodge Percent", foreground=colors[0])
 l3.grid(row=3, column=0, padx=10, pady=10, sticky=W)
+l4 = Label(master=f1, text="Armor Percent", foreground=colors[0])
+l4.grid(row=4, column=0, padx=10, pady=10, sticky=W)
+l5 = Label(master=f1, text="Pierce Percent", foreground=colors[0])
+l5.grid(row=5, column=0, padx=10, pady=10, sticky=W)
 
-e4 = Entry(master=f1, vcmd=command, highlightbackground='#ccc', highlightthickness=1, bd=0)
-e4.grid(row=6, column=1, padx=10, pady=10)
-e5 = Entry(master=f1, vcmd=command, highlightbackground='#ccc', highlightthickness=1, bd=0)
-e5.grid(row=7, column=1, padx=10, pady=10)
 e6 = Entry(master=f1, vcmd=command, highlightbackground='#ccc', highlightthickness=1, bd=0)
-e6.grid(row=8, column=1, padx=10, pady=10)
-l4 = Label(master=f1, text="Health Points", foreground=colors[1])
-l4.grid(row=6, column=0, padx=10, pady=10, sticky=W)
-l5 = Label(master=f1, text="Attack Damage", foreground=colors[1])
-l5.grid(row=7, column=0, padx=10, pady=10, sticky=W)
-l6 = Label(master=f1, text="Dodge Percent", foreground=colors[1])
-l6.grid(row=8, column=0, padx=10, pady=10, sticky=W)
+e6.grid(row=7, column=1, padx=10, pady=10)
+e7 = Entry(master=f1, vcmd=command, highlightbackground='#ccc', highlightthickness=1, bd=0)
+e7.grid(row=8, column=1, padx=10, pady=10)
+e8 = Entry(master=f1, vcmd=command, highlightbackground='#ccc', highlightthickness=1, bd=0)
+e8.grid(row=9, column=1, padx=10, pady=10)
+e9 = Entry(master=f1, vcmd=command, highlightbackground='#ccc', highlightthickness=1, bd=0)
+e9.grid(row=10, column=1, padx=10, pady=10)
+e10 = Entry(master=f1, vcmd=command, highlightbackground='#ccc', highlightthickness=1, bd=0)
+e10.grid(row=11, column=1, padx=10, pady=10)
+l6 = Label(master=f1, text="Health Points", foreground=colors[1])
+l6.grid(row=7, column=0, padx=10, pady=10, sticky=W)
+l7 = Label(master=f1, text="Attack Damage", foreground=colors[1])
+l7.grid(row=8, column=0, padx=10, pady=10, sticky=W)
+l8 = Label(master=f1, text="Dodge Percent", foreground=colors[1])
+l8.grid(row=9, column=0, padx=10, pady=10, sticky=W)
+l9 = Label(master=f1, text="Armor Percent", foreground=colors[1])
+l9.grid(row=10, column=0, padx=10, pady=10, sticky=W)
+l10 = Label(master=f1, text="Pierce Percent", foreground=colors[1])
+l10.grid(row=11, column=0, padx=10, pady=10, sticky=W)
 
-for entry in [e1, e2, e3, e4, e5, e6]:
-    entry.insert(END, inputs[[e1, e2, e3, e4, e5, e6].index(entry)])
+for entry in [e1, e2, e3, e4, e5, e6, e7, e8, e9, e10]:
+    entry.insert(END, inputs[[e1, e2, e3, e4, e5, e6, e7, e8, e9, e10].index(entry)])
     entry.config(validate='key')
 
 update()
